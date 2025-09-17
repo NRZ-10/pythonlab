3fb3ef19d3ca40f5aa5b485010ae29be4fcb8e10
NRZ-10 <niranjan.zr710@gmail.com>
2025-09-17 11:49:34 +0530

    done

---
+++ operations.py
@@ -0,0 +1,17 @@
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+sum=a+b
+difference=a-b
+product=a*b
+division=a/b
+exponent=a**b
+mod=a%b
+intdivision=a//b
+print(f"The arithematic operations bw {a} and {b} is \n")
+print(f"Sum is {sum} \n")
+print(f"Difference is {difference} \n")
+print(f"Product is {product} \n")
+print(f"Divison is {division} \n")
+print(f"Exponent is {exponent}\n")
+print(f"Modulus is {mod}\n")
+print(f"Intdivision is {intdivision}\n")
