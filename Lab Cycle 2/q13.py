953d87b22adbe082475af8a3f95ac3812f9d0218
NRZ-10 <niranjan.zr710@gmail.com>
2025-10-23 23:03:04 +0530

    done

--- Lab Cycle 2/q13.py
+++ Lab Cycle 2/q13.py
@@ -1,15 +1,12 @@
-# Get input from the user
 string = input("Enter a string: ")
 
-# Create an empty dictionary to store character frequencies
 char_freq = {}
 
-# Count each character
 for char in string:
     if char in char_freq:
         char_freq[char] += 1
     else:
         char_freq[char] = 1
-
-# Display the result
+        
 print("Character frequencies:", char_freq)
+
