1e36803107724c26f452cf7ac6754fa5e648155c
NRZ-10 <niranjan.zr710@gmail.com>
2025-10-24 10:24:51 +0530

    done

--- Lab Cycle 3/q2.py
+++ Lab Cycle 3/q2.py
@@ -4,14 +4,6 @@ a, b = 0, 1
 
 print("Fibonacci series:")
 
-if n <= 0:
-    print("Please enter a positive integer.")
-elif n == 1:
-    print(a)
-else:
-    print(a, b, end=" ")
-    for i in range(2, n):
-        c = a + b
-        print(c, end=" ")
-        a = b
-        b = c
+for i in range(n):
+    print(a, end=" ")
+    a,b = b, a + b
